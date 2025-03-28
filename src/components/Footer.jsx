349c6e166d3797e05167494d513bd4cfcf7a03ac
lawaffy <laurawathen@hotmail.com>
2025-03-28 11:36:42 +0000

    update wireframes and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,18 @@
+import logo from "../assets/ncnewz.png";
+
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <img src={logo} alt="logo" style={styles.logo} />
+    </div>
+  );
 }
 
 export default Footer;
+
+const styles = {
+  logo: {
+    width: 50,
+    height: 65,
+  },
+};
